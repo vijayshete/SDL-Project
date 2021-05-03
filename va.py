023b190e6ca42a08214dbad5deb70a87c1cfef37
Vijay shete <67770267+vijayshete@users.noreply.github.com>
2021-05-03 22:12:48 +0530

    Update va.py

--- va.py
+++ va.py
@@ -1,120 +1,119 @@
-import speech_recognition as sr #import impoortant lybrary
-from time import ctime
-import time
+import pyttsx3  # pip install pyttsx3
+import speech_recognition as sr  # pip install speechRecognition
+import datetime
+import wikipedia  # pip install wikipedia
+import webbrowser
 import os
-from gtts import gTTS
-import requests, json
+import smtplib
+
+print("Initializing Jarvis")
+MASTER = "Aman"
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
 
 
-def listen():  #This uses the SpeechRecognition library to activate your machine’s microphone, and then converts the audio to text in the form of a string
+# Speak function will speak/Pronounce the string which is passed to it
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+# This funtion will wish you as per the current time
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    print(hour)
+
+    if 0 <= hour < 12:
+        speak("good morning" + MASTER)
+
+    elif 12 <= hour < 18:
+        speak("good afternoon" + MASTER)
+
+    else:
+        speak("good Evening" + MASTER)
+
+    speak("i am your assistant. How may I help you?")
+
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('kumaraman.rose@gmail.ocm', 'password')
+    server.sendmail("harry@gmail.com", to, content)
+    server.close()
+
+
+# This function will take command from the microphone
+def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("I am listening...")
+        print("Listening...")
         audio = r.listen(source)
-    data = ""
-    try:
-        data = r.recognize_google(audio)
-        print("You said: " + data)
-    except sr.UnknownValueError:
-        print("Google Speech Recognition did not understand audio")
-    except sr.RequestError as e:
-        print("Request Failed; {0}".format(e))
-    return data
-    
-    
-def respond(audioString): #This uses the gTTS library take a string input, prints it then convert the string to an audio file.  This audio file is saved to the local directory and then played by our operating system.
 
-    print(audioString)
-    tts = gTTS(text=audioString, lang='en')
-    tts.save("speech.mp3")
-    os.system("mpg321 speech.mp3")
-    
-    
-def digital_assistant(data):  #Voiced Responses
-    if "how are you" in data:
-        listening = True
-        respond("I am well")
-    if "what time is it" in data:
-        listening = True
-        respond(ctime())
-        
-    if "stop listening" in data:
-        listening = False
-        print('Listening stopped')
-        return listening
-    return listening
-    
-time.sleep(2)
-respond("Hi Dante, what can I do for you?")
-listening = True
-while listening == True:
-    data = listen()
-    listening = digital_assistant(data)
-    
-    
-#Google Maps Query
-def digital_assistant(data):
-    if "how are you" in data:
-        listening = True
-        respond("I am well")
-    if "what time is it" in data:
-        listening = True
-        respond(ctime())
-    if "where is" in data:
-        listening = True
-        data = data.split(" ")
-        location_url = "https://www.google.com/maps/place/" + str(data[2])
-        respond("Hold on Dante, I will show you where " + data[2] + " is.")
-        maps_arg = '/usr/bin/open -a "/Applications/Google Chrome.app" ' + location_url
-        os.system(maps_arg)
-        
-    if "stop listening" in data:
-        listening = False
-        print('Listening stopped')
-        return listening
-    return listening
-    
-    #Weather Query
-    def digital_assistant(data):
-    global listening
-    if "how are you" in data:
-        listening = True
-        respond("I am well")
-    if "what time is it" in data:
-        listening = True
-        respond(ctime())
-    if "where is" in data:
-        listening = True
-        data = data.split(" ")
-        location_url = "https://www.google.com/maps/place/" + str(data[2])
-        respond("Hold on Dante, I will show you where " + data[2] + " is.")
-        maps_arg = '/usr/bin/open -a "/Applications/Google Chrome.app" ' + location_url
-        os.system(maps_arg)
-     if "what is the weather in" in data:
-        listening = True
-        api_key = "Your_API_key"
-        weather_url = "http://api.openweathermap.org/data/2.5/weather?"
-        data = data.split(" ")
-        location = str(data[5])
-        url = weather_url + "appid=" + api_key + "&q=" + location 
-        js = requests.get(url).json() 
-        if js["cod"] != "404": 
-            weather = js["main"] 
-            temp = weather["temp"] 
-            hum = weather["humidity"] 
-            desc = js["weather"][0]["description"]
-            resp_string = " The temperature in Kelvin is " + str(temp) + " The humidity is " + str(hum) + " and The weather description is "+ str(desc)
-            respond(resp_string)
-        else: 
-            respond("City Not Found")
-    if "stop listening" in data:
-        listening = False
-        print('Listening stopped')
-    return listening
-time.sleep(2)
-respond("Hi Dante, what can I do for you?")
-listening = True
-while listening == True:
-    data = listen()
-    listening = digital_assistant(data)
-    
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        query = None
+
+    return query
+
+
+# main program starting
+def main():
+    speak("Initializing Jarvis...")
+    wishMe()
+    query = takeCommand()
+
+    # Logic for executing tasks as per the query
+    if 'wikipedia' in query.lower():
+        speak('searching wikipedia...')
+        query = query.replace("wikipedia", "")
+        results = wikipedia.summary(query, sentences=2)
+        print(results)
+        speak(results)
+
+    elif 'open youtube' in query.lower():
+        # webbrowser.open('youtube.com')
+        url = "youtube.com"
+        chrome_path = 'c:/program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open(url)
+
+    elif 'open google' in query.lower():
+        # webbrowser.open('youtube.com')
+        url = "google.com"
+        chrome_path = 'c:/program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open(url)
+
+    elif 'play music' in query.lower():
+        songs_dir = "C:\\Users\\Dell\\Desktop\\Photos\\audio"
+        songs = os.listdir(songs_dir)
+        print(songs)
+        os.startfile(os.path.join(songs_dir, songs[0]))
+
+    elif 'the time' in query.lower():
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"{MASTER} the time is {strTime}")
+
+    elif 'open code' in query.lower():
+        codePath = "C:\\Users\\Dell\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+        os.startfile(codePath)
+
+    elif 'email to raj' in query.lower():
+        try:
+            speak("what should i send")
+            content = takeCommand()
+            to = "harry@gmail.ocm"
+            sendEmail(to, content)
+            speak("Email has been sent to raj")
+        except Exception as e:
+            print(e)
+
+
+main()
